54634e4042fbde5a9b6027347780678b780645b2
Vedansh <superuser.ntsystems@outlook.com>
2025-03-14 16:28:52 +0530

    fix: auth updates.

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,22 +6,22 @@ dotenv.config();
 const router = express.Router();
 
 const {
-    CLOUDFLARE_ACCOUNT_ID,
-    CLOUDFLARE_R2_ACCESS_KEY_ID,
-    CLOUDFLARE_R2_SECRET_ACCESS_KEY,
-    CLOUDFLARE_R2_BUCKET_NAME
+    VITE_ACCOUNT_ID,
+    VITE_R2_ACCESS_KEY_ID,
+    VITE_R2_SECRET_ACCESS_KEY,
+    VITE_R2_BUCKET_NAME
 } = process.env;
 
-if (!CLOUDFLARE_ACCOUNT_ID || !CLOUDFLARE_R2_ACCESS_KEY_ID || !CLOUDFLARE_R2_SECRET_ACCESS_KEY || !CLOUDFLARE_R2_BUCKET_NAME) {
+if (!VITE_ACCOUNT_ID || !VITE_R2_ACCESS_KEY_ID || !VITE_R2_SECRET_ACCESS_KEY || !VITE_R2_BUCKET_NAME) {
     throw new Error("Missing Cloudflare R2 environment variables.");
 }
 
 router.get("/auth", (_req: express.Request, res: express.Response) => {
     res.json({
-        cloudflareAccountId: CLOUDFLARE_ACCOUNT_ID,
-        cloudflareR2AccessKeyId: CLOUDFLARE_R2_ACCESS_KEY_ID,
-        cloudflareR2SecretAccessKey: CLOUDFLARE_R2_SECRET_ACCESS_KEY,
-        cloudflareR2BucketName: CLOUDFLARE_R2_BUCKET_NAME,
+        cloudflareAccountId: VITE_ACCOUNT_ID,
+        cloudflareR2AccessKeyId: VITE_R2_ACCESS_KEY_ID,
+        cloudflareR2SecretAccessKey: VITE_R2_SECRET_ACCESS_KEY,
+        cloudflareR2BucketName: VITE_R2_BUCKET_NAME,
     });
 });
 
